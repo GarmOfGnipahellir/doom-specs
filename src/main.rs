03bf18f7536dfdab16c1193c4870599233d4c685
Garm <melsom.henrik@gmail.com>
2022-03-09 01:04:13 +0100

    page generation working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use convert_case::{Case, Casing};
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::{fmt::Write as FmtWrite, fs::File, io::Write as IoWrite, path::Path};
+use std::{
+    fs::{create_dir_all, File},
+    io::Write,
+    path::Path,
+};
 
 const DMSP: &'static str = include_str!("dmsp.txt");
 
 fn main() {
     // println!("{}", DMSP);
 
-    for header in get_headers(DMSP) {
-        println!("{header}");
-    }
+    // for header in get_headers(DMSP) {
+    //     println!("{header}");
+    // }
     // for subheader in get_subheaders() {
     //     println!("{subheader}");
     // }
@@ -36,55 +40,89 @@ fn main() {
     let headers = get_headers(DMSP);
     let bodies = get_bodies(DMSP);
 
-    let mut nodes: Vec<Section> = vec![];
+    let mut pages: Vec<Page> = vec![];
+    let mut appendices = Page {
+        header: "Appendices".to_string(),
+        body: "".to_string(),
+        children: vec![],
+    };
     for i in 0..headers.len() {
+        let header = &headers[i];
+        if header == "CONTENTS" {
+            continue;
+        }
+
         let subheaders = get_subheaders(&bodies[i + 1]);
         let subbodies = get_subbodies(&bodies[i + 1]);
 
-        let mut children: Vec<Section> = vec![];
+        let has_body = subheaders.len() != subbodies.len();
+        let body = if has_body { &subbodies[0] } else { "" }.to_string();
+
+        let mut children: Vec<Page> = vec![];
         for j in 0..subheaders.len() {
-            children.push(Section {
+            children.push(Page {
                 header: subheaders[j].clone(),
-                body: bodies[i + 1].clone(),
+                body: subbodies[j + 1].clone(),
                 children: vec![],
             });
         }
 
-        nodes.push(Section {
-            header: headers[i].clone(),
-            body: subbodies[0].clone(),
-            children: vec![],
-        });
+        if headers[i].starts_with("APPENDIX") {
+            appendices.children.push(Page {
+                header: header.clone(),
+                body,
+                children,
+            });
+        } else {
+            pages.push(Page {
+                header: header.clone(),
+                body,
+                children,
+            });
+        }
+    }
+    pages.push(appendices);
+
+    // write pages
+    for page in &pages {
+        page.write_markdown(Path::new("test"));
     }
 
-    // write markdown
-    for node in &nodes {
-        node.write_markdown(Path::new("test"));
+    // write summary
+    for page in &pages {
+        // TODO: generate summary
     }
 }
 
 #[derive(Debug)]
-struct Section {
+struct Page {
     header: String,
     body: String,
-    children: Vec<Section>,
+    children: Vec<Page>,
 }
 
-impl Section {
-    fn write_markdown(&self, root: &Path) {
+impl Page {
+    fn write_markdown(&self, dir: &Path) {
         let header = parse_header(&self.header);
-        let path = if self.children.len() > 0 {
-            root.join(header.to_case(Case::Kebab)).join("README.md")
+
+        let dir = if self.children.len() > 0 {
+            dir.join(header.to_case(Case::Kebab))
         } else {
-            root.join(format!("{}.md", header.to_case(Case::Kebab)))
+            dir.to_path_buf()
+        };
+        create_dir_all(dir.clone()).unwrap();
+
+        let path = if self.children.len() > 0 {
+            dir.join("README.md")
+        } else {
+            dir.join(format!("{}.md", header.to_case(Case::Kebab)))
         };
         let mut file = File::create(path).unwrap();
         writeln!(&mut file, "# {}\n", header).unwrap();
         writeln!(&mut file, "{}", self.body.trim()).unwrap();
 
         for child in &self.children {
-            println!("{}", child.header);
-            child.write_markdown(&root.join(header.to_case(Case::Kebab)));
+            child.write_markdown(&dir);
         }
     }
 }
@@ -123,7 +161,7 @@ fn get_subbodies(text: &str) -> Vec<String> {
 
 fn parse_header(text: &str) -> String {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r".+ \[.+\]: (.+)").unwrap();
+        static ref RE: Regex = Regex::new(r"\[.+\]: (.+)").unwrap();
     }
     RE.captures(text)
         .map(|cap| cap.get(1).unwrap().as_str().to_string())
